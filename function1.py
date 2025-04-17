73abbb0e46163831d4f16095012c74a07385f776
tirumalakanduri <tirumalakandurisudarshansai@gmail.com>
2025-04-16 21:00:59 -0400

    commit

---
+++ function1.py
@@ -0,0 +1,10 @@
+
+def even_odd(number):
+    if number % 2 == 0:
+        print(f"the given number {number} is even")
+    else:
+        print(f"the given number {number} is odd")
+    
+
+number = int(input("enter the number : "))
+even_odd(number)
